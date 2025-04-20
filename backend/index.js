cca1ab5c922b0f951e9afb849f27ae704b6cb0c0
munees-tech <muneeswarann27@gmail.com>
2025-04-20 21:13:10 +0530

    fixed

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDb } from "./src/lib/db.js";
-import authRoute from "./src/routes/auth.roues.js";
+import authRoute from "./src/routes/auth.routes.js";
 import messageRoute from "./src/routes/message.route.js"
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { io,server,app } from "./src/lib/socket.js";
+import { server,app } from "./src/lib/socket.js";
 import path from "path"
 
 dotenv.config();
-
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
@@ -23,9 +22,12 @@ app.use(cookieParser());
 app.use("/api/auth",authRoute);
 app.use("/api/message",messageRoute);
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
-    
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    /* app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    }); */
 }
 
 server.listen(PORT,()=>{
