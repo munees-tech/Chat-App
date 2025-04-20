06db9a6e62d423f974594775448a8ae79d6ad3f4
munees-tech <muneeswarann27@gmail.com>
2025-04-20 21:23:14 +0530

    changed

--- backend/index.js
+++ backend/index.js
@@ -23,10 +23,10 @@ app.use("/api/auth",authRoute);
 app.use("/api/message",messageRoute);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
   
-    /* app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+ /*    app.use("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
     }); */
 }
 
