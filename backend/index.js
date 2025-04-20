c6c0c9eeaaf2cebdfcd967ae9b98f5ce12cd206e
munees-tech <muneeswarann27@gmail.com>
2025-04-20 21:30:43 +0530

    changed

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,38 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDb } from "./src/lib/db.js";
-import authRoute from "./src/routes/auth.routes.js";
-import messageRoute from "./src/routes/message.route.js"
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { server,app } from "./src/lib/socket.js";
-import path from "path"
+
+import path from "path";
+
+import { connectDb } from "./src/lib/db.js";
+
+import authRoutes from "./src/routes/auth.routes.js";
+import messageRoutes from "./src/routes/message.route.js";
+import { app, server } from "./src/lib/socket.js";
 
 dotenv.config();
+
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(cors({
-    origin : "http://localhost:5173",
-    credentials : true,
-}));
-
-app.use(express.json({limit:"10mb"}));
+app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth",authRoute);
-app.use("/api/message",messageRoute);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  
- /*    app.use("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-    }); */
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 }
 
-server.listen(PORT,()=>{
-    console.log(`server is running on ${PORT}`);
-    connectDb();
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDb();
 });
