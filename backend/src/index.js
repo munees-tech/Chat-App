b6c50b34bb96cc96c1f35ca30a2644693c29603f
munees-tech <muneeswarann27@gmail.com>
2025-05-01 21:43:36 +0530

    changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,9 +27,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")))
-  console.log(path.join(__dirname, "../frontend/dist"));
-  app.use(express.static(path.join(__dirname, "../frontend/dist/index.html")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
 }
 
 server.listen(PORT, () => {
